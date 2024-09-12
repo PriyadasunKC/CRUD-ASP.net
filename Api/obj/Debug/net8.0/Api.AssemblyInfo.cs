33e63bd6a2320d3cc9e55c34e2e884ad29b7a0ce
Chathura <cpriydasun@gmail.com>
2024-09-12 21:54:09 +0530

    commit 8

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c3d73e41108d7349acb34dc6183894860cc36d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaa41a216c9d6b12f0ed23ff8bb062ae26eff426")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
